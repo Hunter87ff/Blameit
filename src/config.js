ebb980d785cf3708d24421522dd937a37130fa9a
hunter87ff <huntergaming702@gmail.com>
2025-03-24 22:05:48 +0530

    Update config.js to clarify CLIENT_ID and PRIVATE_KEY usage

--- src/config.js
+++ src/config.js
@@ -4,5 +4,5 @@ configDotenv();
 
 
 export const PORT=process.env.PORT || 3000
-export const CLIENT_ID=process.env.CLIENT_ID
-export const PRIVATE_KEY = process.env.PRIVATE_KEY || fs.readFileSync("./private-key.pem", "utf8");
+export const CLIENT_ID=process.env.CLIENT_ID //use client_id insted of app id
+export const PRIVATE_KEY = process.env.PRIVATE_KEY || fs.readFileSync("./private-key.pem", "utf8"); //private key or the path to the private key
